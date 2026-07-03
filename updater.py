e594cb91d27a6365d66556af90bf78e9d2b8f5db
Soyibjon1 <sobirjonovsoyibjon@yahoo.com>
2026-07-03 13:38:54 +0500

    My updater

--- updater.py
+++ updater.py
@@ -1,15 +1,31 @@
 import os
 import requests
 
+
 REPO    = "Soyibjon1/maktab"
 BRANCH  = "main"
-FILES   = ["Talaba.py", "client_agent.py", "updater.py", "requirements.txt"]
+FILES   = ["Talaba.py", "client_agent.py", "updater.py", "requirements.txt", 'version.txt']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+try:
+    CURRENT_PATH = os.path.join(BASE_DIR, "version.txt")
+    with open(CURRENT_PATH, "r", encoding="utf-8") as f:
+        CURRENT = f.read().strip()
+except Exception as e:
+    print(f"[Updater] Xato-1: {e}")
+    CURRENT = "1.0.0"
 
 def check_and_update() -> bool:
     try:
+        r = requests.get(
+            f"https://raw.githubusercontent.com/{REPO}/{BRANCH}/version.txt",
+            timeout=5,
+        )
+        r.raise_for_status()
+        if r.text.strip() == CURRENT:
+            return False
+
         for filename in FILES:
             r = requests.get(
                 f"https://raw.githubusercontent.com/{REPO}/{BRANCH}/{filename}",
@@ -22,7 +38,7 @@ def check_and_update() -> bool:
 
         return True
     except Exception as e:
-        print(f"[Updater] Xato: {e}")
+        print(f"[Updater] Xato-2: {e}")
         return False
 
 def log(a):
